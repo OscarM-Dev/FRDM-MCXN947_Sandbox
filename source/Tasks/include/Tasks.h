8f80a25352be89945fa56851f7470926264285eb
Oscar Mercado <omercadorico@gmail.com>
2026-02-24 21:46:35 -0600

    Adding task related tools and functions

--- source/Tasks/include/Tasks.h
+++ source/Tasks/include/Tasks.h
@@ -6,7 +6,83 @@
  */
 #ifndef TASKS_H
 #define TASKS_H
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "fsl_common.h"
+#include "fsl_debug_console.h"
+#include "fsl_gpio.h"
+#include "fsl_port.h"
+#include "board.h"
+#include "Queue.h"
 
+/*******************************************************************************
+ * Macros
+ ******************************************************************************/
+#define NUM_TASKS 8
+#define CORE_CLK    150000000
+#define TASK_DELAY_250_MS  250000
+#define TASK_DELAY_500_MS  500000
+#define TASK_DELAY_750_MS  750000
+#define TASK_DELAY_1000_MS 1000000
+
+
+/*******************************************************************************
+ * Typedefs
+ ******************************************************************************/
+typedef void ( *Task ) ( uint32_t );
+
+/*******************************************************************************
+ * Enums
+ ******************************************************************************/
+/**
+ * @brief Task ID enum.
+ * 
+ */
+typedef enum Task_IDs
+{
+    TASK_ID_UNDEFINED_E,
+    TASK_ID_1_E,
+    TASK_ID_2_E,
+    TASK_ID_3_E,
+    TASK_ID_4_E,
+    TASK_ID_5_E,
+    TASK_ID_6_E,
+    TASK_ID_7_E,
+    TASK_ID_8_E
+} Task_IDs_t;
+
+/**
+ * @brief Task status enum.
+ * 
+ */
+typedef enum Task_Status
+{
+    TASK_STATUS_UNDEFINED,
+    TASK_STATUS_READY_E,
+    TASK_STATUS_RUNNING_E,
+    TASK_STATUS_BLOCKED_E
+} Task_Status_t;
+
+/*******************************************************************************
+ * Stucts and Unions.
+ ******************************************************************************/
+/**
+ * @brief Task control struct.
+ * 
+ */
+typedef struct Task_Control
+{
+    Task TaskFunction;
+    Task_IDs_t TaskId;
+    uint32_t BurstTime;     //us.
+    Task_Status_t Status;
+} Task_Control_t;
+
+/*******************************************************************************
+ * Public function prototypes.
+ ******************************************************************************/
+bool Task_Init( Task_Control_t* ctrl );
 
 
 #endif
--- source/Tasks/src/Tasks.c
+++ source/Tasks/src/Tasks.c
@@ -8,3 +8,206 @@
  * Includes
  ******************************************************************************/
 #include "Tasks.h"
+
+/*******************************************************************************
+ * Private function prototypes.
+ ******************************************************************************/
+static void System_Task1( uint32_t burstTime );
+static void System_Task2( uint32_t burstTime );
+static void System_Task3( uint32_t burstTime );
+static void System_Task4( uint32_t burstTime );
+static void System_Task5( uint32_t burstTime );
+static void System_Task6( uint32_t burstTime );
+static void System_Task7( uint32_t burstTime );
+static void System_Task8( uint32_t burstTime );
+
+/*******************************************************************************
+ * Public function definitions.
+ ******************************************************************************/
+/**
+ * @brief This function initialices the array control struct for the scheduler tasks.
+ * 
+ * @param ctrl Pointer to control structs.
+ * @retval Operation result. 
+ */
+bool Task_Init( Task_Control_t* ctrl )
+{
+    bool result = false;
+
+    if ( ctrl != NULL )
+    {
+        void* tasksAddr[NUM_TASKS] = { System_Task1, System_Task2, System_Task3, System_Task4,
+                System_Task5, System_Task6, System_Task7, System_Task8 };
+
+        for ( uint8_t i = 0; i < NUM_TASKS; i++ )
+        {
+            ctrl[i].TaskFunction = tasksAddr[i];
+            ctrl[i].TaskId = i + 1;
+            ctrl[i].Status = TASK_STATUS_BLOCKED_E;
+            ctrl[i].BurstTime = TASK_DELAY_250_MS;
+        }
+                
+        result = true;
+    }
+
+    return result;
+}
+
+/*******************************************************************************
+ * Private function definitions.
+ ******************************************************************************/
+/**
+ * @brief Task 1 function.
+ * 
+ * @param burstTime Delay for task in us.
+ */
+static void System_Task1( uint32_t burstTime )
+{
+    PRINTF( "Executing Task 1\r\n" );
+
+    //RGB LED off.
+    LED_RED_OFF();
+    LED_GREEN_OFF();
+    LED_BLUE_OFF();
+
+    //Burst time.
+    SDK_DelayAtLeastUs( burstTime, CORE_CLK );
+}
+
+/**
+ * @brief Task 2 function.
+ * 
+ * @param burstTime Delay for task in us.
+ */
+static void System_Task2( uint32_t burstTime )
+{
+    PRINTF( "Executing Task 2\r\n" );
+
+    //RGB LED RED on.
+    LED_RED_OFF();
+    LED_GREEN_OFF();
+    LED_BLUE_OFF();
+    LED_RED_ON();
+
+    //Burst time.
+    SDK_DelayAtLeastUs( burstTime, CORE_CLK );
+}
+
+/**
+ * @brief Task 3 function.
+ * 
+ * @param burstTime Delay for task in us.
+ */
+static void System_Task3( uint32_t burstTime )
+{
+    PRINTF( "Executing Task 3\r\n" );
+
+    //RGB LED Yellow on.
+    LED_RED_OFF();
+    LED_GREEN_OFF();
+    LED_BLUE_OFF();
+    LED_RED_ON();
+    LED_GREEN_ON();
+
+    //Burst time.
+    SDK_DelayAtLeastUs( burstTime, CORE_CLK );
+}
+
+/**
+ * @brief Task 4 function.
+ * 
+ * @param burstTime Delay for task in us.
+ */
+static void System_Task4( uint32_t burstTime )
+{
+    PRINTF( "Executing Task 4\r\n" );
+
+    //RGB LED Magenta on.
+    LED_RED_OFF();
+    LED_GREEN_OFF();
+    LED_BLUE_OFF();
+    LED_RED_ON();
+    LED_BLUE_ON();
+
+    //Burst time.
+    SDK_DelayAtLeastUs( burstTime, CORE_CLK );
+}
+
+/**
+ * @brief Task 5 function.
+ * 
+ * @param burstTime Delay for task in us.
+ */
+static void System_Task5( uint32_t burstTime )
+{
+    PRINTF( "Executing Task 5\r\n" );
+
+    //RGB LED Green on.
+    LED_RED_OFF();
+    LED_GREEN_OFF();
+    LED_BLUE_OFF();
+    LED_GREEN_ON();
+
+    //Burst time.
+    SDK_DelayAtLeastUs( burstTime, CORE_CLK );
+}
+
+/**
+ * @brief Task 6 function.
+ * 
+ * @param burstTime Delay for task in us.
+ */
+static void System_Task6( uint32_t burstTime )
+{
+    PRINTF( "Executing Task 6\r\n" );
+
+    //RGB LED Cyan on.
+    LED_RED_OFF();
+    LED_GREEN_OFF();
+    LED_BLUE_OFF();
+    LED_GREEN_ON();
+    LED_BLUE_ON();
+
+    //Burst time.
+    SDK_DelayAtLeastUs( burstTime, CORE_CLK );
+}
+
+/**
+ * @brief Task 7 function.
+ * 
+ * @param burstTime Delay for task in us.
+ */
+static void System_Task7( uint32_t burstTime )
+{
+    PRINTF( "Executing Task 7\r\n" );
+
+    //RGB LED Blue on.
+    LED_RED_OFF();
+    LED_GREEN_OFF();
+    LED_BLUE_OFF();
+    LED_BLUE_ON();
+
+    //Burst time.
+    SDK_DelayAtLeastUs( burstTime, CORE_CLK );
+}
+
+/**
+ * @brief Task 8 function.
+ * 
+ * @param burstTime Delay for task in us.
+ */
+static void System_Task8( uint32_t burstTime )
+{
+    PRINTF( "Executing Task 8\r\n" );
+
+    //RGB LED White on.
+    LED_RED_OFF();
+    LED_GREEN_OFF();
+    LED_BLUE_OFF();
+    LED_RED_ON();
+    LED_GREEN_ON();
+    LED_BLUE_ON();
+
+    //Burst time.
+    SDK_DelayAtLeastUs( burstTime, CORE_CLK );
+}
